ff2973fd742244d930da18513ceacc79ab7f8f72
xoyzo <chukwubuikemc.alt@gmail.com>
2026-06-13 16:23:15 -0400

    Update __init__.py

--- special_bonuses/migrations/__init__.py
+++ special_bonuses/migrations/__init__.py
@@ -1,10 +1,1 @@
-from typing import TYPE_CHECKING
 
-from .cog import SpecialBonuses
-
-if TYPE_CHECKING:
-    from ballsdex.core.bot import BallsDexBot
-
-
-async def setup(bot: "BallsDexBot") -> None:
-    await bot.add_cog(SpecialBonuses(bot))

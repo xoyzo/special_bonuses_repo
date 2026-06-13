ffb8a25c885c5da9778db3be993eb93790b83bac
xoyzo <chukwubuikemc.alt@gmail.com>
2026-06-13 16:14:14 -0400

    Create pyproject.py

---
+++ pyproject.py
@@ -0,0 +1,16 @@
+[project]
+name = "special_bonuses"
+version = "1.0.0"
+description = "Configurable stat bonuses and luck systems for Ballsdex specials"
+license = "MIT"
+license-files = ["LICENSE"]
+authors = [
+    { name = "xoyzo", email = "cheapcandiez@gmail.com" },
+]
+readme = "README.md"
+# dependencies = [
+#     "ballsdex>=3.0.0",
+# ]
+
+[project.urls]
+Homepage = "https://github.com/xoyzo/special_bonuses"
